69fd884ab4b883385b22221b72045783f5728065
marius-faber <mariusfaber98@gmail.com>
2022-07-15 10:45:04 +0200

    first commit

---
+++ src/Resources/app/administration/src/module/sw-cms/blocks/four-column/component/index.js
@@ -0,0 +1,8 @@
+import template from './sw-cms-block-four-column.html.twig';
+import './sw-cms-block-four-column.scss';
+
+const { Component } = Shopware;
+
+Component.register('sw-cms-block-four-column', {
+    template,
+});
